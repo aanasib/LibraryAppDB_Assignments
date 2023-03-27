e8fefe9b9e08d8c9be8a077e96eee3c11445deb3
Attia_Nasib <aanasib@cydeo.com>
2023-03-27 15:03:28 -0400

    Finished master branch

---
+++ target/test-classes/library.sql
@@ -0,0 +1,27 @@
+
+-- US01
+select count(id) from users; -- 1855
+select count(distinct id) from users;
+
+-- US02
+select * from users;
+
+-- US 03
+select count(*) from book_borrow
+where is_returned=0;
+
+-- US04
+select * from books where name = 'AttiaNasib';
+
+-- US05
+
+-- US06
+
+-- US07
+
+
+
+
+
+
+
